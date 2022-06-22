ff2e555f581b825f14ac879fe98e5d2a5478ef44
Lukas Scharmer <lscharmer@databay.de>
2022-06-22 12:51:33 +0200

    Add permission check for dashboard settings

--- Services/Dashboard/Administration/classes/class.ilObjDashboardSettingsGUI.php
+++ Services/Dashboard/Administration/classes/class.ilObjDashboardSettingsGUI.php
@@ -216,6 +216,7 @@ class ilObjDashboardSettingsGUI extends ilObjectGUI
         $side_panel = $this->side_panel_settings;
         
         if (!$ilAccess->checkAccess('write', '', $this->object->getRefId())) {
+            ilUtil::sendFailure($this->lng->txt('no_permission'), true);
             $ilCtrl->redirect($this, "editSettings");
         }
 
@@ -404,6 +405,11 @@ class ilObjDashboardSettingsGUI extends ilObjectGUI
         $request = $this->request;
         $lng = $this->lng;
         $ctrl = $this->ctrl;
+
+        if (!$this->access->checkAccess('write', '', $this->object->getRefId())) {
+            ilUtil::sendFailure($lng->txt('no_permission'), true);
+            $ctrl->redirect($this, $redirect_cmd);
+        }
 
         $form = $this->getViewSettingsForm($view);
         $form = $form->withRequest($request);
